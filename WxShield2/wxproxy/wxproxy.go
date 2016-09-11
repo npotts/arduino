2d289dbdcc56f820383a9b8430c52bf100e90037
npotts <npotts@users.noreply.github.com>
2016-09-10 23:12:00 -0600

    Changed Serial port libraries and it got better

--- WxShield2/wxproxy/wxproxy.go
+++ WxShield2/wxproxy/wxproxy.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 	"github.com/alecthomas/kingpin"
 	_ "github.com/cockroachdb/pq"
-	"github.com/dustin/go-rs232"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
+	"github.com/tarm/serial"
+	"os"
 	"strings"
 )
 
 var (
-	create = "CREATE TABLE %s.%s (id SERIAL, timestamp TIMESTAMP DEFAULT now(), pressure FLOAT, tempa FLOAT, tempb FLOAT, humidity FLOAT, ptemp FLOAT, htemp FLOAT, battery FLOAT, indx INT)"
+	create = "CREATE TABLE IF NOT EXISTS %s.%s  (id SERIAL, timestamp TIMESTAMP DEFAULT now(), pressure FLOAT, tempa FLOAT, tempb FLOAT, humidity FLOAT, ptemp FLOAT, htemp FLOAT, battery FLOAT, indx INT)"
 
-	app            = kingpin.New("wxProxy", "Shovel data from an arduino into a cockroachdb somewhere else")
+	app            = kingpin.New("wxProxy", "Shovel data from an arduino into a postgres/cockroachdb")
 	dataSourceName = app.Flag("dataSource", "Where should we connect to and yelp at (usually a string like 'postgresql://root@dataserver:26257?sslmode=disable')").Short('s').Default("postgresql://root@chipmunk:26257?sslmode=disable").String()
 	database       = app.Flag("database", "The database to aim at").Short('d').Default("wx").String()
 	raw            = app.Flag("table", "The database table to fire into").Short('t').Default("raw").String()
@@ -23,10 +24,11 @@ var (
 
 type inserter struct {
 	db  *sqlx.DB
-	ser *rs232.SerialPort
+	ser *serial.Port
 }
 
-func mustOpen(port *rs232.SerialPort, err error) *rs232.SerialPort {
+func getPort() *serial.Port {
+	port, err := serial.OpenPort(&serial.Config{Name: *device, Baud: 57600, Size: 8, Parity: serial.ParityNone, StopBits: 1})
 	if err != nil {
 		panic(errors.Wrap(err, "Unable to open serial device"))
 	}
@@ -34,35 +36,40 @@ func mustOpen(port *rs232.SerialPort, err error) *rs232.SerialPort {
 }
 
 func new() *inserter {
-	i := &inserter{
+	return &inserter{
 		db:  sqlx.MustConnect("postgres", *dataSourceName),
-		ser: mustOpen(rs232.OpenPort(*device, 57600, rs232.S_8N1)),
+		ser: getPort(),
 	}
-	i.db.MustExec(fmt.Sprintf(create, *database, *raw))
-	return i
 }
 
 //poll forever will look for newlines and assume whatever it got was a full on SQL query worthy of executing.
 func (ins *inserter) poll() {
+	ins.db.MustExec(fmt.Sprintf(create, *database, *raw))
 	rdr := bufio.NewReader(ins.ser)
+	lines := 0
 	for {
 		line, err := rdr.ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
 		}
-		go ins.insert(line)
+		lines++
+		fmt.Printf("%d lines ingested\r", lines)
+		if strings.Contains(line, "INSERT INTO") {
+			go ins.insert(line)
+		}
 	}
 }
 
 func (ins *inserter) insert(data string) {
-	data = strings.Replace(data, "wx", fmt.Sprintf("%d.%d", *database, *raw), 1)
+	data = fmt.Sprintf(data, *database, *raw)
 	if _, err := ins.db.Exec(data); err != nil {
 		fmt.Println(errors.Wrap(err, "Unable to insert"))
+		return
 	}
 }
 
 func main() {
-	kingpin.Parse()
+	app.Parse(os.Args[1:])
 	i := new()
 	i.poll()
 }
