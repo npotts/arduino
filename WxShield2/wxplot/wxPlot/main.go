630bb68da19a1dd200f6635591a37a33f578380b
npotts <npotts@users.noreply.github.com>
2016-09-11 18:48:24 -0600

    Added main.go

--- WxShield2/wxplot/wxPlot/main.go
+++ WxShield2/wxplot/wxPlot/main.go
@@ -9,15 +9,14 @@ import (
 
 var (
 	app            = kingpin.New("wxPlot", "Plot data pulled from postgres/cockroachdb")
-	dataSourceName = app.Flag("dataSource", "Where should we connect to and yelp at (usually a string like 'postgresql://root@dataserver:26257?sslmode=disable')").Short('s').Default("postgresql://root@chipmunk:26257?sslmode=disable").String()
+	dataSourceName = app.Flag("dataSource", "Where should we connect to and yank data from (usually a string like 'postgresql://root@dataserver:26257?sslmode=disable')").Short('s').Default("postgresql://root@chipmunk:26257?sslmode=disable").String()
 	database       = app.Flag("database", "The database to aim at").Short('d').Default("wx").String()
 	raw            = app.Flag("table", "The database table read raw data from").Short('t').Default("raw").String()
-	// daily          = app.Flag("daily", "").Short('a').Default("true").Bool()
 )
 
 func main() {
 	app.Parse(os.Args[1:])
-	i := wxplot.New(dataSourceName, database, raw)
+	i := wxplot.New(*dataSourceName, *database, *raw)
 	i.Hourly()
 	// i.poll()
 }
