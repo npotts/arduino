8699545aa50426be14a575f141a033bc4ac5232f
npotts <npotts@users.noreply.github.com>
2016-09-15 20:00:18 -0600

    Add option to shovel output files to a particular directory

--- WxShield2/wxplot/wxPlot/main.go
+++ WxShield2/wxplot/wxPlot/main.go
@@ -12,11 +12,12 @@ var (
 	dataSourceName = app.Flag("dataSource", "Where should we connect to and yank data from (usually a string like 'postgresql://root@dataserver:26257?sslmode=disable')").Short('s').Default("postgresql://root@chipmunk:26257?sslmode=disable").String()
 	database       = app.Flag("database", "The database to aim at").Short('d').Default("wx").String()
 	raw            = app.Flag("table", "The database table read raw data from").Short('t').Default("raw").String()
+	dir            = app.Flag("output-dir", "Where should the output files be shoveled").Short('t').Default(".").String()
 )
 
 func main() {
 	app.Parse(os.Args[1:])
 	i := wxplot.New(*dataSourceName, *database, *raw)
-	i.WriteFile("hourly.html",i.Hourly())
-	i.WriteFile("weekly.html",i.Weekly())
+	i.WriteFile(*dir+"/hourly.html", i.Hourly())
+	i.WriteFile(*dir+"/weekly.html", i.Weekly())
 }
