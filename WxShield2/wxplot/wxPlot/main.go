17ab269304fd0c8f5b4edd2a7fe19d23f5ffd41e
npotts <npotts@users.noreply.github.com>
2016-09-16 08:00:00 -0600

    Fixed duplicate short tags, which inhibited parsing

--- WxShield2/wxplot/wxPlot/main.go
+++ WxShield2/wxplot/wxPlot/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/alecthomas/kingpin"
-	_ "github.com/cockroachdb/pq"
 	"github.com/npotts/arduino/WxShield2/wxplot"
 	"os"
 )
@@ -12,11 +11,13 @@ var (
 	dataSourceName = app.Flag("dataSource", "Where should we connect to and yank data from (usually a string like 'postgresql://root@dataserver:26257?sslmode=disable')").Short('s').Default("postgresql://root@chipmunk:26257?sslmode=disable").String()
 	database       = app.Flag("database", "The database to aim at").Short('d').Default("wx").String()
 	raw            = app.Flag("table", "The database table read raw data from").Short('t').Default("raw").String()
-	dir            = app.Flag("output-dir", "Where should the output files be shoveled").Short('t').Default(".").String()
+	dir            = app.Flag("output-dir", "Where should the output files be shoveled").Short('p').Default(".").String()
 )
 
 func main() {
-	app.Parse(os.Args[1:])
+	if _, err := app.Parse(os.Args[1:]); err != nil {
+		panic(err)
+	}
 	i := wxplot.New(*dataSourceName, *database, *raw)
 	i.WriteFile(*dir+"/hourly.html", i.Hourly())
 	i.WriteFile(*dir+"/weekly.html", i.Weekly())
