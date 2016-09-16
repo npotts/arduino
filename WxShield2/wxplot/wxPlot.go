665faa51dc0078e2639c7b51a5cfc7050bc8cb49
npotts <npotts@users.noreply.github.com>
2016-09-15 19:51:16 -0600

    Heavily decimate the amount of data to be less than 150 points

--- WxShield2/wxplot/wxPlot.go
+++ WxShield2/wxplot/wxPlot.go
@@ -3,8 +3,8 @@ package wxplot
 import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
+	"io/ioutil"
 	"time"
-  "io/ioutil"
 
 	_ "github.com/cockroachdb/pq" //Postgres sql hook
 )
@@ -25,16 +25,20 @@ func New(dataSourceName, database, table string) *PlotUtil {
 }
 
 func (p *PlotUtil) databyRange(start, end time.Time) (f frames, err error) {
-	//find out how many samples we are dealing with
-	i, samples := 0, 750
-	if err = p.db.Get(&i, fmt.Sprintf("SELECT COUNT(*) FROM %s.%s WHERE timestamp > $1 and timestamp < $2", p.database, p.table), start, end); err != nil {
-		return
-	}
- 	i = int(i / samples) //max of 750ish samples
-	//query := fmt.Sprintf("SELECT * FROM %s.%s WHERE timestamp > $1 AND timestamp < $2  ORDER BY timestamp LIMIT %d OFFSET %d;", p.database, p.table, samples, i)
 	query := fmt.Sprintf("SELECT * FROM %s.%s WHERE timestamp > $1 AND timestamp < $2  ORDER BY timestamp", p.database, p.table)
-	err = p.db.Select(&f, query, start, end)
-  return
+	if err = p.db.Select(&f, query, start, end); err != nil {
+		return frames{}, err
+	}
+	//probably not the most efficient manner here
+	decimate := len(f) / 250
+	newf := frames{}
+	for i, frame := range f {
+		if i%decimate == 0 {
+			frame.Timestamp = frame.Timestamp.Local()
+			newf = append(newf, frame)
+		}
+	}
+	return newf, nil
 }
 
 /*Hourly geerates 24-hour plots that start at midnight the day before now and end at midnight*/
@@ -42,18 +46,27 @@ func (p PlotUtil) Hourly() string {
 	end := time.Now()
 	start := time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, time.Local).UTC()
 	data, _ := p.databyRange(start.UTC(), end)
-  return data.html("Daily")
+	return data.html("Daily")
 }
 
 /*Weekly generates 7-day plots*/
 func (p PlotUtil) Weekly() string {
-  end := time.Now().UTC().Local()
-  start := time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, end.Location())
-  start = start.Add(-24 * time.Duration(end.Weekday()) * time.Hour);
+	end := time.Now().UTC().Local()
+	start := time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, end.Location())
+	start = start.Add(-24 * time.Duration(end.Weekday()) * time.Hour)
 	data, _ := p.databyRange(start.UTC(), end.UTC())
 	return data.html("Weekly")
 }
 
+/*Monthly generates Calandar month plots*/
+func (p PlotUtil) Monthly() string {
+	end := time.Now().UTC().Local()
+	start := time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, end.Location())
+	start = start.Add(-24 * time.Duration(end.Weekday()) * time.Hour)
+	data, _ := p.databyRange(start.UTC(), end.UTC())
+	return data.html("Monthy")
+}
+
 func (p *PlotUtil) WriteFile(filename, data string) {
-  ioutil.WriteFile(filename, []byte(data), 0777);
+	ioutil.WriteFile(filename, []byte(data), 0777)
 }
