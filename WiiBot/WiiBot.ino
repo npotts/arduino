6626346eaf6c3bf36f63de6ef48e8b4bc1e44c3e
npotts <npotts@users.noreply.github.com>
2016-10-29 18:38:24 -0600

    remove leftover comment block

--- WiiBot/WiiBot.ino
+++ WiiBot/WiiBot.ino
@@ -1,22 +1,3 @@
-/*
- Fading
-
- This example shows how to fade an LED using the analogWrite() function.
-
- The circuit:
- * LED attached from digital pin 9 to ground.
-
- Created 1 Nov 2008
- By David A. Mellis
- modified 30 Aug 2011
- By Tom Igoe
-
- http://www.arduino.cc/en/Tutorial/Fading
-
- This example code is in the public domain.
-
- */
-
  #include "MotorCtrl.h"
 
  MotorCtrl left(6,7);
