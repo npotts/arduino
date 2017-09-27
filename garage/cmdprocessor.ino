c51ae6efb9624dfa610ecf6bffc33a468a514d7e
npotts <npotts@no-reply.users.github.com>
2017-09-26 21:54:55 -0600

    Add MIT license to sources

--- garage/cmdprocessor.ino
+++ garage/cmdprocessor.ino
@@ -1,3 +1,27 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016-2017 Nick Potts
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "cmdprocessor.h"
 
 /*cmd_status returns the full status which is a comma
@@ -40,7 +64,7 @@ void cmd_help() {
   );
 }
 
-/*cmd_proc takes one of the following single ASCII character 
+/*cmd_proc takes one of the following single ASCII character
 incoming 'commands' and does something with them. It explicitly
 clears the queue after enacting on one.
 
@@ -95,4 +119,3 @@ void cmd_proc() {
     Serial.println(Prompt);
   }
 }
-
--- garage/hwcfg.h
+++ garage/hwcfg.h
@@ -1,5 +1,29 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016-2017 Nick Potts
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #pragma once
-/* 
+/*
 # Diagram
 
 DoorAtFloor and DoorAtCeiling instruct this firmware the range the door should operate in, as well as which way is 'up'
@@ -7,7 +31,7 @@ DoorAtFloor and DoorAtCeiling instruct this firmware the range the door should o
 --Ceiling---|  <--- DoorAtCeiling value
             |
   Garage    |   Door Operating range
-  or inside |  
+  or inside |
             |
 ---Ground---|  <--- DoorAtFloow value
 
@@ -27,7 +51,7 @@ unsigned int pdelta = DoorAtFloor - DoorAtCeiling;
 
 //Pin Values
 #define A2DPin A0
-#define RelayPin 13 
+#define RelayPin 13
 #define RelayDur 300  //300ms
 #define RelayIdle LOW //Idling should be low
 #define RelaySet HIGH //when set, should set the pin high
@@ -36,4 +60,3 @@ unsigned int pdelta = DoorAtFloor - DoorAtCeiling;
 #define MotionWait 300      // How long to wait between position samples
 
 #define Prompt ">>"
-
